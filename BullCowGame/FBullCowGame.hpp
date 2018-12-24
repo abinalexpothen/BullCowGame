d3772ff3b91014dfdaafacf680e3d5bbf235ace3
Abin Alex Pothen <abinalexpothen@gmail.com>
2018-12-23 20:37:41 -0500

    My experiments with the Udemy course on game development using Unreal Engine

--- BullCowGame/FBullCowGame.hpp
+++ BullCowGame/FBullCowGame.hpp
@@ -9,7 +9,7 @@
 #ifndef FBullCowGame_hpp
 #define FBullCowGame_hpp
 
-#include <stdio.h>
+#include <iostream>
 
 #endif /* FBullCowGame_hpp */
 
@@ -23,6 +23,9 @@ public:
     void ProvideClue(); // TODO provide a clue by revealing one of the minimum
     void PrintRules(); // TODO display the rules of the game
     int ModifyMaxTries(); // TODO Customize the current maximum number of tries
+    
+    bool IsGameWon();
+    bool CheckGuessValidity(); // TODO return a more rich return value
     
     
 // please try and ignore this; focus on iterface
